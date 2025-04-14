43763375de1baac4c1560a9325d2edf8c7e4ebc7
haddadtalel <talel.haddad5@gmail.com>
2025-04-14 14:57:18 +0100

    this commit is before update the About us section and integrat the dynamic content preview

--- src/Controller/Administrator/DashboardController.php
+++ src/Controller/Administrator/DashboardController.php
@@ -5,7 +5,10 @@ namespace App\Controller\Administrator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Enity\AboutUs;
+use App\Enity\ContactUs;
+use App\Enity\Home;
+use App\Enity\OurServices;
 class DashboardController extends AbstractController
 {
     #[Route('/administrator/dashboard', name: 'app_administrator_dashboard')]
--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -10,16 +10,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\QuoteDescRepository;
 class QuoteController extends AbstractController
 {
     #[Route('/quote', name: 'app_quote')]
-    public function index(Request $request, EntityManagerInterface $entityManager , AboutUsRepository $aboutUs ): Response
+    public function index(Request $request, EntityManagerInterface $entityManager , AboutUsRepository $aboutUs , QuoteDescRepository $quoteDesc ): Response
     {
         $quote = new Quote();
         $form = $this->createForm(QuoteType::class, $quote);
         $form->handleRequest($request);
         $aboutUs = $aboutUs->find(1);
+        $quoteDesc = $quoteDesc->find(1);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($quote);
             $entityManager->flush();
@@ -29,6 +30,7 @@ class QuoteController extends AbstractController
         }
 
         return $this->render('quote/quote.html.twig', [
+            'quoteDesc' => $quoteDesc,
             'form' => $form->createView(),
             'aboutUs' => $aboutUs,
         ]);
